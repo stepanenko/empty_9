a03c0b996db0abbd58c1e960a62954e21e27dd53
stepanenko <serhiy.stepanenko@aiesec.net>
2020-06-07 19:10:17 +0300

    small changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Brad Gatsby Crash`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Create great Gatsby project with this default starter.`,
+    author: `stepanenco`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
